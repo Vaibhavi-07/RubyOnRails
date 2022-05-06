e4f51aece08b93529ceb71c5a89c64c15a02d052
Vaibhavi Yadav <84709348+Vaibhavi-07@users.noreply.github.com>
2022-05-06 15:12:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-echo "Creating new project"
-rails new RoR1
-echo "installing bundle"
+echo "Remove old data"
+sudo rm -rf /ruby
+echo "cloning git repository"
+git clone https://github.com/Vaibhavi-07/RubyOnRails.git
+echo "remove gemfile"
+cd /ruby && sudo rm -rf Gemfile.lock
+echo "Install bundler"
 bundle install
-echo "Start rails server"
+echo "start server"
 rails server -b 0.0.0.0
